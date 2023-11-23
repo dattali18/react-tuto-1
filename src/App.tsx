1e66416bfc109c863a2b8c18b0ee3537fe62e697
dattali18 <74492594+dattali18@users.noreply.github.com>
2023-11-23 15:39:33 +0200

    added README.md file

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,9 @@
-// import Navbar from "./Components/Navbar";
+import NavbarVertical from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <div className="navbar-vertical">
-        <div className="navbar-logo-items">
-          <NavbarLogo />
-
-          <div className="navbar-item">
-            <NavbarItem iconName="home" title="Home" />
-            <NavbarItem iconName="list" title="Tasks" />
-            <NavbarItem iconName="setting" title="Settings" />
-          </div>
-        </div>
-
-        <NavbarItem iconName="user" title="Logout" />
-      </div>
-    </>
+    <NavbarVertical />
   );
 }
 
 export default App;
-
-interface NavBarItem_Props {
-  iconName: string;
-  title: string;
-}
-
-function NavbarItem({ iconName, title }: NavBarItem_Props) {
-  return (
-    <>
-      <div className="navbar-items">
-        <img src={"/" + iconName + ".svg"} className="navbar-item-icon" />
-        <p className="navbar-item-text">{title}</p>
-      </div>
-    </>
-  );
-}
-
-function NavbarLogo() {
-  return (
-    <>
-      <div className="navbar-logo">
-        <img src="/logo.svg" className="navbar-logo-icon" />
-        <p className="navbar-logo-text">Task manager</p>
-      </div>
-    </>
-  );
-}
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,37 +1,49 @@
-export default function Navbar() {
+interface NavBarItem_Props {
+  iconName: string;
+  title: string;
+}
+
+function NavbarItem({ iconName, title }: NavBarItem_Props) {
   return (
     <>
-      <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-        </div>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <div className="navbar-items">
+        <img src={"/" + iconName + ".svg"} className="navbar-item-icon" />
+        <p className="navbar-item-text">{title}</p>
+      </div>
     </>
   );
 }
+
+function NavbarLogo() {
+  return (
+    <>
+      <div className="navbar-logo">
+        <img src="/logo.svg" className="navbar-logo-icon" />
+        <p className="navbar-logo-text">Task manager</p>
+      </div>
+    </>
+  );
+}
+
+function NavbarVertical() {
+  return (
+    <>
+      <div className="navbar-vertical">
+        <div className="navbar-logo-items">
+          <NavbarLogo />
+
+          <div className="navbar-item">
+            <NavbarItem iconName="home" title="Home" />
+            <NavbarItem iconName="list" title="Tasks" />
+            <NavbarItem iconName="setting" title="Settings" />
+          </div>
+        </div>
+
+        <NavbarItem iconName="user" title="Logout" />
+      </div>
+    </>
+  );
+}
+
+
+export default NavbarVertical;
